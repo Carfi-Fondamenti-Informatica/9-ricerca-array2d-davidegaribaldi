225c21afeabaf30f2f7a2868ff990768a701880b
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-05-09 19:02:15 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 int funz(char nome[10][20],char ricerca[20])
 {
-    int dim1=0,c=0,posizione=-1,a=0;
+    int dim1=0,dim=0,c=0,posizione=-1,a=0;
     for(int i=0;i<10;i++)
     {
         for(int j=0;j<20;j++)
@@ -20,29 +20,33 @@ int funz(char nome[10][20],char ricerca[20])
             ricerca[j] = (char) a;
         }
     }
-    for(int j=0;j<20;j++)
-    {
-        if(ricerca[j]!=0)
+    for (int j = 0; j < 20; j++) {
+        if(ricerca[j] !=0)
         {
-            dim1++;
-        }
-        else
-        {
-            break;
+            dim++;
         }
     }
 
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < dim1; j++) {
-            if (nome[i][j] == ricerca[j]) {
-                c++;
-            }
+        for (int j = 0; j < 20; j++) {
+         if(nome[i][j] !=0)
+         {
+             dim1++;
+         }
+        }
+        if(dim1>=dim) {
+            for (int j = 0; j < dim1; j++) {
+                if (nome[i][j] == ricerca[j]) {
+                    c++;
+                }
 
+            }
+            if (c == dim1) {
+                posizione = i + 1;
+            }
+            c = 0;
+            dim1 = 0;
         }
-        if (c == dim1) {
-            posizione = i+1;
-        }
-        c=0;
     }
     return posizione;
 }
