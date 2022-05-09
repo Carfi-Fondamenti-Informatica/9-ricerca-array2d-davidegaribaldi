7a7878d547e148f09f0e634762ffd6422b662ca0
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-05-09 18:10:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+   char nome[10][20],nomericerca[20];
+   for(int i=0;i<10;i++)
+   {
+   cin>>nome[i];
+   }
+   for(int i=0;i<20;i++)
+   {
+       nomericerca[i]=0;
+   }
+   cin>>nomericerca;
+   if(funz(nome,nomericerca)==-1)
+   {
+   cout<<"non presente";
+   }
+   else
+   {
+    cout<<funz(nome,nomericerca)   ;
+   }
 
-int main(){
-  return 0;
+
+    return 0;
 }
+
